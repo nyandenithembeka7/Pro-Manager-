a477c4685f9243a55b19b36de9a9c1b9a7dd8905
nyandenithembeka7 <nyandenithembeka7@gmail.com>
2026-09-06 13:28:17 +0200

    Update bizbrain_app.py

--- bizbrain_app.py
+++ bizbrain_app.py
@@ -338,8 +338,8 @@ def main_dashboard():
 def main():
     init_db()
     
-    if 'logged_in' not in st.session_state:
-        st.session_state['logged_in'] = False
+    # BYPASS LOGIN FOR DEMO - Remove this when going live
+st.session_state['logged_in'] = True
     
     if not st.session_state['logged_in']:
         auth_page()
